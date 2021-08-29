4033c79efc419bc9b1e82cbad81a3c8724213839
Armando <pcr942@my.utsa.edu>
2021-08-29 13:00:50 -0500

    succesfully change pixel to cyan at coordinates [0,1]

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ from numpy import asarray
 
 
 def openImage(fileImage):
+
+    # with Image.open(fileImage) as image:
+    #     pixel = image.load()
+
+    # image.show()
+    # print(pixel[4, 4])
+    # pixel[4, 4] = (0,255, 255)
+    # print(pixel[4, 4])
+    # image.show()
+
+
     img = Image.open(fileImage)
     print(img)
 
@@ -28,27 +39,30 @@ def openImage(fileImage):
     image_pillow.show()
 
     # iterate through pixels to try and change the color of a single pixel
-    # for column in range(image_pillow.size[0]):
-    #   for row in range(image_pillow.size[1]):
-    #      pixels[column, row] = (0, 0, 0)
-
-    # image_pillow.show()
-
-    # Approach 2
-    image_attempt = Image.open(fileImage, 'r')
-
-    # extract the pixel values
-    pixel_values = list(image_attempt.getdata())
-
-    # Fix list arrangement from tuples to list
-    pixel_values_arrange = [x for sets in pixel_values for x in sets]
-
-    print(pixel_values_arrange[1:3])
-    pixel_values_arrange[1:3] = (255, 0, 0)
-    coordinate = x, y = 0, 0
+    for column in range(image_pillow.size[0]):
+        for row in range(image_pillow.size[1]):
+            if pixels[column, row] != (255, 0, 0):
+                pixels[0, 1] = (0, 255, 255)
 
     # show new image
     image_pillow.show()
+    #image_pillow.save("output.bmp")
+
+    # Approach 2
+    #image_attempt = Image.open(fileImage, 'r')
+
+    # extract the pixel values
+    #pixel_values = list(image_attempt.getdata())
+
+    # Fix list arrangement from tuples to list
+    #pixel_values_arrange = [x for sets in pixel_values for x in sets]
+
+    #print(pixel_values_arrange[1:3])
+    #pixel_values_arrange[1:3] = (255, 0, 0)
+    coordinate = x, y = 0, 0
+
+    # show new image
+    #image_pillow.show()
 
 
 # Press the green button in the gutter to run the script.
